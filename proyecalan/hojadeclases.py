efa1bc6dfa73b02ab73c9625e7688466679a68c8
diazalan12 <packo.diaz12@gmail.com>
2023-11-14 19:51:50 -0300

    sadsadas

--- proyecalan/hojadeclases.py
+++ proyecalan/hojadeclases.py
@@ -17,3 +17,83 @@ class Dinero():
         
 
 
+class Destinos():
+    def__init__(self,destino,horario):
+        Destino = destino
+        Horario = horario
+        checkbox = getattr(self,Horario,None)
+        checkbox1 = getattr(self,Destino,None)
+        if checkbox.isChecked() and checkbox1.isChecked():
+            self.precio_pasaje_LR= 1800
+            self.precio_pasajes.setText("$" + str(self.precio_pasaje_LR))
+            i=0
+            while i < len(self.asientos_larioja_m): #aca habilitamos y deshabilitamos botones
+
+                #print(self.asientos_larioja_m[i])
+                if self.asientos_larioja_m[i] == 'l':
+                    self.b[i].setEnabled(True)
+                    print (f'habilitando el asiento {i}')
+                    
+                else:
+                    self.b[i].setEnabled(False)
+                    print(f'desabilitando el asiento {i}')
+                i = i + 1
+            self.asi() 
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+while 
+
+s
+
+
+
+i = 0
+while i < len(self.asientos_larioja_m):
+    
+    if self.asientos_larioja_m(i) == 'O':
+        variable_name = 'checkBox_{i}'
+        checkbox = getattr(self,variable_name,None) 
+        if checkbox and checkbox.isChecked():
+                #print(f'entrando al lugar de la lista {x}')
+                self.asientos_larioja_m[i] = 'l'                                             
+                checkbox.setEnabled(True)
+                checkbox.setChecked(False)
+
+    
+    
+    
+    
+    
+    
+    i += 1
+    
+
+
+
+
+
+
+
+
